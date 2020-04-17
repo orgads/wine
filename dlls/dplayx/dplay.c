12fa10f87cc1bca4f36597637753da9c1b5d41a9
Orgad Shaneh <orgads@gmail.com>
2020-04-17 10:05:52 +0300

    dplayx: System players are the ones who must not be listed
    The name server is a system player, and the server player is just another normal player
    
    Based on patch by Ismael Barros <razielmine@gmail.com>

--- dlls/dplayx/dplay.c
+++ dlls/dplayx/dplay.c
@@ -2394,11 +2394,10 @@ static HRESULT WINAPI IDirectPlay4Impl_EnumGroupPlayers( IDirectPlay4 *iface, DP
     /* Walk the players in this group */
     for( plist = DPQ_FIRST( gdata->players ); ; plist = DPQ_NEXT( plist->players ) )
     {
-        /* We do not enum the name server or app server as they are of no
+        /* We do not enum any system players as they are of no
          * consequence to the end user.
          */
-        if ( ( plist->lpPData->dpid != DPID_NAME_SERVER ) &&
-                ( plist->lpPData->dpid != DPID_SERVERPLAYER ) )
+        if ( plist->lpPData->dwFlags & DPLAYI_PLAYER_SYSPLAYER )
         {
             /* FIXME: Need to add stuff for flags checking */
             if ( !enumplayercb( plist->lpPData->dpid, DPPLAYERTYPE_PLAYER,
